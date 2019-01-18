1aa373fa7bbaa8405725109e5d572dafe61c6642
Nolan Tait <nolanjtait@gmail.com>
2019-01-17 22:02:14 -0800

    Initial commit

---
+++ lib/omniauth/open/guilds.rb
@@ -0,0 +1,10 @@
+require "omniauth/open/guilds/version"
+
+module Omniauth
+  module Open
+    module Guilds
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/open/guilds/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Open
+    module Guilds
+      VERSION = "0.1.0"
+    end
+  end
+end
