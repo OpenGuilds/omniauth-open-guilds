3d391c0e2d995b5a500e4e079fe3641d4a86193e
Nolan Tait <nolanjtait@gmail.com>
2019-01-22 01:35:54 -0800

    Fixes bad file structure

--- lib/omniauth/omniauth-open-guilds.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-#
-require "omniauth/open_guilds/version"
-require 'omniauth/open_guilds'
--- lib/omniauth/strategies/open_guilds.rb
+++ lib/omniauth/strategies/open_guilds.rb
@@ -1,4 +1,4 @@
-require 'omniauth-oauth2'
+require 'omniauth/strategies/oauth2'
 require 'json'
 
 module OmniAuth
